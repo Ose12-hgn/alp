a8199778539fb7dc7c22442e78cf02db7b749d68
Angga Dhammika Jaya <165568852+TanggaRusakk@users.noreply.github.com>
2025-05-26 12:31:10 +0700

    Update Queue.java

--- Home/src/Model/Queue.java
+++ Home/src/Model/Queue.java
@@ -1,5 +1,36 @@
 package Model;
 
-public class Queue {
-   
+import java.util.LinkedList;
+import Interface.Queueable;
+
+public class Queue implements Queueable{
+    private LinkedList list = new LinkedList();
+
+    @Override
+    public void enqueue(Object e) {
+        list.addLast(e);
+    }
+
+    @Override
+    public Object dequeue() {
+        if (!list.isEmpty()) {
+            return list.removeFirst();
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public Object peek() {
+        if (!list.isEmpty()) {
+            return list.getFirst();
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
 }
