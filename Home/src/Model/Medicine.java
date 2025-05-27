2d983f4a46053309d385c8fc0eead8c735e0da83
Ose12-hgn <fjosehagen@student.ciputra.ac.id>
2025-05-27 13:03:08 +0700

    Update Medicine.java

--- Home/src/Model/Medicine.java
+++ Home/src/Model/Medicine.java
@@ -1,63 +1,121 @@
 package Model;
 
-public class Medicine {
-  // properties in medicine
-    private String name;
-    private String manufacturer;
-    private double price;
-    private String expiryDate;
-    private int quantity;
-  
-    // constructors
-    public Medicine(String name, String manufacturer, double price, String expiryDate, int quantity) {
-        this.name = name;
-        this.manufacturer = manufacturer;
-        this.price = price;
-        this.expiryDate = expiryDate;
-        this.quantity = quantity;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Medicine extends Item {
+    private LocalDate tanggalKadaluarsa;
+    private String dosis; // Example "Zyloric 300mg"
+    private String bentukSediaan; // Example: "Tabs Syrup"
+    private boolean membutuhkanResep;
+    private String produsen; // Company Make
+    private String kategoriObat; // Example: "Antibiotics Vitamin"
+    private String petunjukPenyimpanan;
+    private boolean isRacikan; // Racik or not
+
+    // Constructor
+    public Medicine(String itemID, String namaItem, String deskripsi, double harga, int stok,
+                    LocalDate tanggalKadaluarsa, String dosis, String bentukSediaan,
+                    boolean membutuhkanResep, String produsen, String kategoriObat,
+                    String petunjukPenyimpanan, boolean isRacikan) {
+        super(itemID, namaItem, deskripsi, harga, stok);
+        this.tanggalKadaluarsa = tanggalKadaluarsa;
+        this.dosis = dosis;
+        this.bentukSediaan = bentukSediaan;
+        this.membutuhkanResep = membutuhkanResep;
+        this.produsen = produsen;
+        this.kategoriObat = kategoriObat;
+        this.petunjukPenyimpanan = petunjukPenyimpanan;
+        this.isRacikan = isRacikan;
     } // asking for parameter
 
-    public String getName() {
-      return name;
-    }
-  
-    public void setName(String name) {
-      this.name = name;
+    public LocalDate getTanggalKadaluarsa() {
+        return tanggalKadaluarsa;
     }
 
-    public String getManufacturer() {
-      return manufacturer;
-    }
-  
-    public void setManufacturer(String manufacturer) {
-      this.manufacturer = manufacturer;
+    public String getDosis() {
+        return dosis;
     }
 
-    public double getPrice() {
-      return price;
-    }
-  
-    public void setPrice(double price) {
-      this.price = price;
+    public String getBentukSediaan() {
+        return bentukSediaan;
     }
 
-    public String getExpiryDate() {
-      return expiryDate;
-    }
-  
-    public void setExpiryDate(String expiryDate) {
-      this.expiryDate = expiryDate;
+    public boolean isMembutuhkanResep() {
+        return membutuhkanResep;
     }
 
-    public int getQuantity() {
-      return quantity;
+    public String getProdusen() {
+        return produsen;
     }
-    public void setQuantity(int quantity) {
-      this.quantity = quantity;
+
+    public String getKategoriObat() {
+        return kategoriObat;
     }
+
+    public String getPetunjukPenyimpanan() {
+        return petunjukPenyimpanan;
+    }
+
+    public boolean isRacikan() {
+        return isRacikan;
+    }
+
+    public void setTanggalKadaluarsa(LocalDate tanggalKadaluarsa) {
+        this.tanggalKadaluarsa = tanggalKadaluarsa;
+    }
+
+    public void setDosis(String dosis) {
+        this.dosis = dosis;
+    }
+
+    public void setBentukSediaan(String bentukSediaan) {
+        this.bentukSediaan = bentukSediaan;
+    }
+
+    public void setMembutuhkanResep(boolean membutuhkanResep) {
+        this.membutuhkanResep = membutuhkanResep;
+    }
+
+    public void setProdusen(String produsen) {
+        this.produsen = produsen;
+    }
+
+    public void setKategoriObat(String kategoriObat) {
+        this.kategoriObat = kategoriObat;
+    }
+
+    public void setPetunjukPenyimpanan(String petunjukPenyimpanan) {
+        this.petunjukPenyimpanan = petunjukPenyimpanan;
+    }
+
+    public void setRacikan(boolean racikan) {
+        isRacikan = racikan;
+    }
+
+    public boolean isKadaluarsa() {
+        return LocalDate.now().isAfter(this.tanggalKadaluarsa);
+    } //getter setter
 
     @Override
     public String toString() {
-        return name + " (" + manufacturer + "), Expires: " + expiryDate + ", Price: " + price + ", Quantity: " + quantity;
-    } // display the information
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM yyyY");
+        String tanggalKadaluarsaStr = (tanggalKadaluarsa != null) ? tanggalKadaluarsa.format(formatter) : "N/A";
+
+        return "Medicine{" +
+               "itemID='" + getItemID() + '\'' +
+               ", namaItem='" + getNamaItem() + '\'' +
+               ", harga=Rp" + String.format("%.2f", getHarga()) +
+               ", stok=" + getStok() +
+               ", tanggalKadaluarsa=" + tanggalKadaluarsaStr +
+               ", dosis='" + dosis + '\'' +
+               ", bentukSediaan='" + bentukSediaan + '\'' +
+               ", membutuhkanResep=" + membutuhkanResep +
+               ", produsen='" + produsen + '\'' +
+               ", kategoriObat='" + kategoriObat + '\'' +
+               ", petunjukPenyimpanan='" + petunjukPenyimpanan + '\'' +
+               ", isRacikan=" + isRacikan +
+               (isKadaluarsa() ? ", STATUS: KADALUARSA" : "") +
+               '}';
+    } // printing the medicine
 }
